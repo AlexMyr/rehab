b4b06ae73212455d2df44a64f09982e552b3bcdd
Alex <alex@miralex.com.ua>
2012-06-22 18:14:42 +0300

    Add translate for this page.

--- php/client_delete.php
+++ php/client_delete.php
@@ -11,12 +11,16 @@ $ft->define(array('main' => "client_delete.html"));
 
 //$dbu = new mysql_db();
 
-$ft->assign('CLIENT_NAME',$glob['client_name']);
+$tags = get_template_tag($glob['pag'], $glob['lang']);
+foreach($tags as $name => $row){
+  $ft->assign($name, $row);
+}
 
 $site_meta_title=$meta_title.get_meta($glob['pag'], $glob['lang'], 'title');
 $site_meta_keywords=$meta_keywords.get_meta($glob['pag'], $glob['lang'], 'keywords');
 $site_meta_description=$meta_description.get_meta($glob['pag'], $glob['lang'], 'description');
 
+$ft->assign('CLIENT_NAME',$glob['client_name']);
 $ft->assign('MESSAGE', get_error($glob['error'],$glob['success']));
 $ft->parse('CONTENT','main');
 return $ft->fetch('CONTENT');
