cf0b9b3f4484859e701b638aa64eab21eb9240a2
Alex <alex@miralex.com.ua>
2012-07-17 16:17:58 +0300

    Change contact mail message.

--- classes/cls_contact.php
+++ classes/cls_contact.php
@@ -9,6 +9,12 @@ class contact
   function contact()
   {
     $this->dbu=new mysql_db;
+	
+	$root_path = dirname(dirname(__FILE__));
+	//require_once ($root_path."/misc/security_lib.php");
+	//require_once ($root_path."/misc/stlib.php");
+	require_once ($root_path.'/classes/class.phpmailer.php');        
+	include_once ($root_path."/classes/class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
   }
 
 
@@ -36,8 +42,9 @@ class contact
   function send_contact_us_notiffication_mail(&$ld)
   {
 	global $site_url,$script_path, $site_name,$contact_thankyou_page_id;
-	//$mail=ADMIN_EMAIL;
-	$mail="support@rehabmypatient.com";
+	
+	$from="support@rehabmypatient.com";
+	
 	$body='New Contact From '.$site_name.' website
 
 Contact Informations:
@@ -49,13 +56,48 @@ Message:
 ------------------------------------------------
 '.$ld['comments'].'
 ';
+
+	$body = nl2br($body);
+	$mail = new PHPMailer();
+	$mail->Mailer = 'sendmail';
+	$mail->IsHTML(true);
+	$mail->IsSMTP(); // telling the class to use SMTP
+	$mail->SMTPDebug = 1; // enables SMTP debug information (for testing)
+	$mail->SMTPAuth = true; // enable SMTP authentication
+	$mail->Host = SMTP_HOST; // sets the SMTP server
+	$mail->Port = SMTP_PORT; // set the SMTP port for the GMAIL server
+	$mail->Username = SMTP_USERNAME; // SMTP account username
+	$mail->Password = SMTP_PASSWORD; // SMTP account password
+
+	$mail->SetFrom($ld['email'], $ld['email']);
+
+	$subject = $message_data['subject'];
+	$mail->Subject = $ld['subject'];
+	$mail->Body = $body;
+
+	$mail->AddAddress($to, $to);
+	$mail->Send();
 	
-	
-	$header.= "From: ".$ld['email']." \n";
-	$header.= "Content-Type: text\n";
-	$mail_subject=$ld['subject'];		   
-	@mail ( $mail , $mail_subject, $body , $header);
-	
+	//$mail=ADMIN_EMAIL;	
+//	$mail="support@rehabmypatient.com";
+//	$body='New Contact From '.$site_name.' website
+//
+//Contact Informations:
+//Name: '.$ld['name'].'
+//Email: '.$ld['email'].'
+//Phone Number: '.$ld['phone'].'
+//           
+//Message:
+//------------------------------------------------
+//'.$ld['comments'].'
+//';
+//	
+//	
+//	$header.= "From: ".$ld['email']." \n";
+//	$header.= "Content-Type: text\n";
+//	$mail_subject=$ld['subject'];		   
+//	@mail ( $mail , $mail_subject, $body , $header);
+//	
 	return true;
   }
 
